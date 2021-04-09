bb2fbf938e2a91c6f51db4b6f4e2913fba557aae
ryannbrown <44414542+ryannbrown@users.noreply.github.com>
2021-04-09 11:06:06 -0400

    remove error message

--- server.js
+++ server.js
@@ -391,11 +391,11 @@ app.post('/api/upload', function (req, res, next) {
 
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+// app.use(function(req, res, next) {
+//   var err = new Error('Not Found');
+//   err.status = 404;
+//   next(err);
+// });
 
 
 if (process.env.NODE_ENV === "production") {
