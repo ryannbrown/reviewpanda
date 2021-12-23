eb968d9824b27677efcdb31a894418439f805463
ryannbrown <44414542+ryannbrown@users.noreply.github.com>
2021-12-23 12:40:38 -0600

    filtering out interventions and resources

--- controllers/getCats.js
+++ controllers/getCats.js
@@ -1,17 +1,17 @@
 const handleCatsFetch = (req, res, db) => {
     console.log('we are here')
-    // const {id } = req.params;
     db('all_tests').distinct('panda_cat')
       .then(cat => {
-          console.log(cat)
+        for (var i = cat.length - 1; i >= 0; --i) {
+          if (cat[i].panda_cat == "Interventions" || cat[i].panda_cat =='Resources') {
+              cat.splice(i,1);
+          }
+      }
+
         if (cat.length) {
-          // console.log(cat)
           res.json(cat)
         } 
         
-        // else {
-        //   res.status(400).json([])
-        // }
       })
       .catch(err => res.status(400).json(err))
   }
